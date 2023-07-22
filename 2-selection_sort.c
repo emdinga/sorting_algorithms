cb856ec69713ed37857ec3a9c89b4f26effd5a87
Emdinga <emdinga@gmail.com>
2023-07-22 11:19:10 -0700

    selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, m_idx;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		m_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[m_idx])
+				m_idx = j;
+		}
+		if (m_idx != i)
+		{
+			swap(&array[i], &array[m_idx]);
+			print_array(array, size);
+		}
+	}
+}
