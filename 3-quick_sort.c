04c12290bc41726057ef9b336a56cb68f9887ac6
Emdinga <emdinga@gmail.com>
2023-07-22 12:22:12 -0700

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,81 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two elements in an array
+ *
+ * @a: Pointer to the first element
+ * @b: Pointer to the second element
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * lomuto_partition - Partitions the array using the Lomuto scheme
+ *
+ * @array: The array to be partitioned
+ * @low: Index of the low partition boundary
+ * @high: Index of the high partition boundary
+ * @size: Number of elements in @array
+ *
+ * Return: Index of the pivot
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+/**
+ * quick_sort_helper - Recursive helper function for Quick Sort
+ *
+ * @array: The array to be sorted
+ * @low: Index of the low boundary
+ * @high: Index of the high boundary
+ * @size: Number of elements in @array
+ */
+void quick_sort_helper(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = lomuto_partition(array, low, high, size);
+		quick_sort_helper(array, low, pivot - 1, size);
+		quick_sort_helper(array, pivot + 1, high, size);
+	}
+}
+/**
+ * quick_sort - Sorts an array of integers in ascending order
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quick_sort_helper(arrat, 0, size - 1, size);
+}
