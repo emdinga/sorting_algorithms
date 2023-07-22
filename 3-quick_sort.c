a1646e9409bfbf983856cd196964ca6834eb7c0d
Emdinga <emdinga@gmail.com>
2023-07-22 12:26:40 -0700

    quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -77,5 +77,5 @@ void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
 		return;
-	quick_sort_helper(arrat, 0, size - 1, size);
+	quick_sort_helper(array, 0, size - 1, size);
 }
--- sort.h
+++ sort.h
@@ -28,6 +28,9 @@ void bubble_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quick_sort_helper(int *array, int low, int high, size_t size);
 
 
 #endif
