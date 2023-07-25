dc7b2b5a30a48888e1c44c2e1fe73702032da354
Emdinga <emdinga@gmail.com>
2023-07-25 12:23:17 -0700

    counting

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -5,4 +5,39 @@
  * @array: The array to be sorted
  * @size: Number of elements in the array
  */
+void counting_sort(int *array, size_t size)
+{
+	int n, j, *count_array, *a;
+	size_t i;
 
+	if (!array || size < 2)
+		return;
+	n = array[0];
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > n)
+			n = array[i];
+	}
+	count_array = calloc((n + 1), sizeof(int));
+	for (i = 0; i < size; i++)
+	{
+		count_array[array[i]]++;
+	}
+	for (j = 1; j < n; j++)
+	{
+		count_array[j + 1] += count_array[j];
+	}
+	print_array(count_array, n + 1);
+	a = malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++)
+	{
+		count_array[array[i]]--;
+		a[count_array[array[i]]] = array[i];
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = a[i];
+	}
+	free(a);
+	free(count_array);
+}
